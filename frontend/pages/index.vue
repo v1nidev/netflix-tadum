e5afcb00f5f4b9d8ce713e258c50d6b483838864
Vinicius Rocha <vinicius.rocha@the9level.com>
2020-10-16 13:38:56 -0300

    Separate ref from home file

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,53 +1,20 @@
 <template>
-  <main class="home">
-    <ResponsiveImage :sources="image.src" :lazyload="false" :alt="image.alt" />
-    <LazyImage :src="image2.src" alt="" role="presentation" class="icon-image" />
+  <main class="home is-clipped">
+    home
+    <!-- <nuxt-link to="/b">go to b</nuxt-link> -->
+    <internal-page />
   </main>
 </template>
 
 <script>
-import ResponsiveImage from '~/components/shared/ResponsiveImage'
-import CardSlider from '~/components/shared/CardSlider'
-import baseImage from '~/utils/baseImage'
+
 
 export default {
-  head () {
-    return this.$createHeadFunction(
-      {
-        title: 'title',
-        description: ''
-      }, 
-      null, 
-      this.$route.path, 
-      null
-    );
-  },
 
-  components: {
-    ResponsiveImage,
-    // CardSlider,
-  },
-
-  data() {
-    return {
-      image: baseImage([
-        {src:'/images/page_1/section_1/mobile.png, /images/page_1/section_1/mobile@2x.png 2x', query: '(max-width: 1023px)'},
-        {src:'/images/page_1/section_1/desktop.png, /images/page_1/section_1/desktop@2x.png 2x', query: ''}
-      ], 'Alternative text'),
-
-      image2: baseImage('/images/shared/success.svg', 'Success', 'Success'),
-    }
-  }
 }
 
 </script>
 
 <style lang="scss" scoped>
-@import "~bulma/sass/utilities/mixins";
 @import "~assets/css/overrides";
-
-.home{
-  overflow: hidden;
-}
-
 </style>
