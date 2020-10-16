5b7732e8cefb3c0bcf83c8b1770638917ccddf65
Vinicius Rocha <vinicius.rocha@the9level.com>
2020-10-16 18:58:44 -0300

    Add page switch

---
+++ frontend/pages/_index/_switch.vue
@@ -0,0 +1,14 @@
+<template>
+  <component v-if="component" :is="component" />
+</template>
+
+<script>
+export default {
+  computed: {
+    component() {
+      // return () => import(`~/components/internal-page/index.vue`);
+      return () => import(`~/components${this.$route.path}/index.vue`);
+    }
+  },
+}
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,20 +1,24 @@
 <template>
   <main class="home is-clipped">
-    home
-    <!-- <nuxt-link to="/b">go to b</nuxt-link> -->
-    <internal-page />
+    HOME
   </main>
 </template>
 
 <script>
-
-
 export default {
-
+  head () {
+    return this.$createHeadFunction(
+      {
+        title: 'home',
+        description: 'home desc'
+      },
+      null, 
+      this.$route.path, 
+      null
+    );
+  },
 }
-
 </script>
 
-<style lang="scss" scoped>
-@import "~assets/css/overrides";
+<style lang="scss">
 </style>
