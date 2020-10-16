7c0e7ecce4d57fdf29db9adf9bffad02fc6e6c99
Vinicius Rocha <vinicius.rocha@the9level.com>
2020-10-16 18:59:24 -0300

    Add demonstration pages

---
+++ frontend/components/internal-page/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="">
+    <h1 class="title">
+      internal-page
+    </h1>
+  </main>
+</template>
+
+<script>
+export default {
+  head () {
+    return this.$createHeadFunction(
+      {
+        title: 'internal page',
+        description: 'internal page desc'
+      },
+      null, 
+      this.$route.path, 
+      null
+    );
+  },
+}
+</script>
---
+++ frontend/components/internal-page/nested-page/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="">
+    <p>
+      nested internal-page
+    </p>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
