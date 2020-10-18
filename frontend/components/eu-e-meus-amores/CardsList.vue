f6620feda83ddc6ef5288d6e155e01ade19e916e
Olavo Parno <olavoparno@gmail.com>
2020-10-18 02:06:57 -0300

    feat(me-n-my-loves): add eu e meus amores page

---
+++ frontend/components/eu-e-meus-amores/CardsList.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <article class="box-container has-background-pink" v-for="(card, i) in cardsList" :key="i">
+      <section class="section-container">
+        <VideoFrame
+          :video-id="card.videoId"
+        />
+        <header class="section-header-text-in-a-box">
+          {{ card.headerText }}
+        </header>
+        <footer class="section-footer-text" v-html="card.footerText" />
+      </section>
+    </article>
+  </div>
+</template>
+
+<script>
+import VideoFrame from "~/components/shared/VideoFrame"
+
+export default {
+  components: {
+    VideoFrame
+  },
+  props: {
+    cardsList: {
+      type: Array,
+      required: true
+    }
+  },
+  mounted(){
+    console.log(this.$props.cardsList)
+  }
+};
+</script>
+
+<style  lang="scss" scoped>
+@import "~assets/css/overrides";
+
+.box-container {
+  border: 1.2px solid black;
+  border-radius: 20px;
+
+  &:not(:last-child) {
+    margin-bottom: $size-5 - 5;
+  }
+
+  & .video-frame {
+    height: 272px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+
+    & /deep/.play-btn {
+      svg > circle {
+        fill: $pink;
+      }
+    }
+
+    @media (min-width: $tablet) {
+      height: 400px;
+    }
+
+    @media (min-width: $desktop) {
+      height: 600px;
+    }
+  }
+
+  & .section-header-text-in-a-box {
+    margin: ($size-7 + 5) ($size-7 + 10) 0;
+    font-size: 32px;
+    font-family: Grennete Pro;
+    font-weight: 400;
+    line-height: 39px;
+    letter-spacing: -0.01em;
+    max-width: 650px;
+    @media (min-width: $tablet) {
+      margin: ($size-7 + 5) auto 0;
+    }
+  }
+
+& .section-footer-text {
+    padding: $size-7 + 5 2.625rem;
+    font-size: 17px;
+    font-family: Grennete Pro;
+    font-weight: 400;
+    line-height: 21px;
+    letter-spacing: -0.01em;
+  }
+}
+</style>
---
+++ frontend/components/eu-e-meus-amores/index.vue
@@ -0,0 +1,152 @@
+<template>
+  <main class="eu-e-meus-amores is-clipped p-b-32 p-l-16 p-r-16">
+    <div class="container">
+      <h1 class="title">
+        {{ title }}
+      </h1>
+      <h2 class="description has-text-pink">
+        {{ description }}
+      </h2>
+      
+      <article class="box-container">
+        <section class="section-container">
+          <ResponsiveImage
+            :sources="looseCard.imageSet.src"
+            :lazyload="false"
+            :alt="looseCard.imageSet.alt"
+            imgClass="image-block"
+          />
+          <header class="section-header-text">
+            Quando a gente divide o tempo entre crush e migues, 24horas parece pouco, né? Mas bora lá que esse problema é do tipo “vira essa boca pra cá”. Se organizar direitinho, isso só acontece nas séries e filmes.
+          </header>
+        </section>
+      </article>
+
+      <CardsList :cardsList="cardsList" />
+
+      <Share text-class="share-text" button-class="share-button" />
+    </div>
+  </main>
+</template>
+
+<script>
+import { title, description, looseCard, cardsList } from "./constants";
+
+import baseImage from "~/utils/baseImage";
+import ResponsiveImage from "~/components/shared/ResponsiveImage";
+import CardsList from "~/components/eu-e-meus-amores/CardsList";
+
+export default {
+  head() {
+    return this.$createHeadFunction(
+      {
+        title: title,
+        description: description,
+      },
+      null,
+      this.$route.path,
+      null
+    );
+  },
+
+  // optional component declarations
+  components: {
+    ResponsiveImage,
+    CardsList
+  },
+
+  data() {
+    return {
+      title,
+      description,
+      looseCard,
+      cardsList,
+      // all options: https://flickity.metafizzy.co/options.html
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~assets/css/overrides";
+
+.eu-e-meus-amores {
+  text-align: center;
+  color: black;
+
+  & .container {
+    margin-top: $size-5 + 4;
+  }
+
+  & /deep/.box-container {
+    border-radius: $size-7;
+    & img {
+      border-radius: $size-7;
+      border: 1.2px solid black;
+    }
+  }
+
+  & .title {
+    font-size: 14px;
+    font-family: Netflix Sans;
+    line-height: 16px;
+    letter-spacing: 0.03em;
+    text-transform: uppercase;
+  }
+
+  & .description {
+    margin-bottom: 2rem;
+    font-size: $size-3 + 10;
+    font-family: Agipo;
+    font-weight: bold;
+    line-height: 70px;
+    letter-spacing: -0.01em;
+    -webkit-text-stroke-width: 1.5px;
+    -webkit-text-stroke-color: black;
+  }
+
+  & .section-container {
+    & .section-header-text {
+      margin: ($size-7 + 5) ($size-8 - 6) $size-5;
+      font-size: 17px;
+      font-family: Grennete Pro;
+      font-weight: 400;
+      line-height: 21px;
+      letter-spacing: -0.01em;
+
+      @media (min-width: $tablet) {
+        margin: ($size-7 + 5) auto $size-5;
+      }
+
+      @media (min-width: $desktop) {
+        padding: 0  $size-5;
+      }
+    }
+  }
+
+  /deep/.share-text {
+    line-height: 16px;
+    letter-spacing: 0.03em;
+    text-transform: uppercase;
+    font-weight: 600;
+  }
+
+  /deep/.button.share-button {
+    background-color: transparent;
+    color: $black;
+    border: 2px solid black;
+    &:hover {
+      color: $pink;
+    }
+  }
+
+  .scroll-top-btn {
+    background: $black;
+    border-color: $pink;;
+  }
+
+  .scroll-top-btn .icon{
+    stroke: $pink;
+  }
+}
+</style>
