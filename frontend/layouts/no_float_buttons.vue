8cb429283b8d8c47a4fc63a8c4be4ecefcf416e5
Vinicius Rocha <vinicius.rocha@the9level.com>
2020-10-17 23:41:12 -0300

    Add float btns to default layout

---
+++ frontend/layouts/no_float_buttons.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="default">
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,33 @@
 <template>
   <main class="home is-clipped">
-    <!--  -->
+    HOME
+    <br>
+    <nuxt-link to="/internal-page">
+      go to "internal page"
+    </nuxt-link>
+    <br>
+    <nuxt-link to="/internal-page/nested-page">
+      go to "nexted internal page"
+    </nuxt-link>
+    <br>
+    <nuxt-link to="/reference">
+      go to "reference"
+    </nuxt-link>
+    <!-- <NavCard to="/other">
+      go to "other page"
+    </NavCard> -->
+    <!-- <LazyInternalPage /> -->
   </main>
 </template>
 
 <script>
 export default {
+  layout: 'no_float_buttons',
+
   head () {
     return this.$createHeadFunction(
       {
-        title: 'home',
+        title: 'HOME',
         description: 'home desc'
       },
       null, 
@@ -17,6 +35,8 @@ export default {
       null
     );
   },
+
+  // transition: "intro"
 }
 </script>
 
