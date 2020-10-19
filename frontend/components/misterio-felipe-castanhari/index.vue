5a549359d9494c57ca76b74039af36b8cbd47486
Vinicius Rocha <vinicius.rocha@the9level.com>
2020-10-19 10:07:13 -0300

    Divide Head/Body on Castanhari page

--- frontend/components/misterio-felipe-castanhari/index.vue
+++ frontend/components/misterio-felipe-castanhari/index.vue
@@ -1,40 +1,8 @@
 <template>
   <main class="mistery m-0-a p-t-8">
-    <section class="container">
-      <img src="~/static/images/shared/close.svg" alt="close" class="close-icon">
-
-      <h3 class="has-text-centered m-b-24 p-t-32 top-title"> 
-        OS DOIS LADOS 
-      </h3>
-
-      <LetteringBanner 
-        class="is-relative banner" 
-        :lettering="'Mistérioooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo'"
-      >
-        <ResponsiveImage 
-          class="banner-sun"
-          :sources="images[0].src" 
-          :lazyload="false" 
-          :alt="images[0].alt" 
-        />
-        <ResponsiveImage 
-          class="banner-planet"
-          :sources="images[1].src" 
-          :lazyload="false" 
-          :alt="images[1].alt" 
-        />
-        <ResponsiveImage 
-          class="banner-portrait"
-          :sources="images[2].src" 
-          :lazyload="false" 
-          :alt="images[2].alt" 
-        />
-      </LetteringBanner>
-      
-      <p class="explanation has-text-centered p-x-24">
-        Só o que o Felipe Castanhari não explica (ainda) é 2020. Sobre o resto – ciência, história, como sobreviver ao EAD –, vc pode perguntar
-      </p>
-    </section>
+    <div class="container">
+      <MisterioFelipeCastanhariHeader />
+    </div>
 
     <section class="container">
       <p class="has-text-centered paragraph border-top is-size-7 p-t-8">
@@ -151,8 +119,6 @@
     <MyList :cards="myList" />
 
     <Share />
-
-    <img src="~/static/images/misterio-felipe-castanhari/top.svg" alt="top" class="top">
   </main>
 </template>
 
@@ -227,45 +193,11 @@ export default {
 .mistery{
   background-color: #00A651;
 
-  .close-icon{
-    height: 45px;
-    position: absolute;
-    top: 12px;
-    right: 18px;
-  }
-
   .container{
     padding: 0px 20px;
   }
 
-  .top-title{
-    color: #000;
-    font-family: Netflix Sans;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 16px;
-  }
-
   .banner{
-
-    &-sun{
-      position: absolute;
-      top: 85px;
-      right: 5rem;
-    }
-
-    &-planet{
-      position: absolute;
-      top: 150px;
-      left: 0.2rem;
-    }
-
-    &-portrait{
-      position: absolute;
-      bottom: 0px;
-      right: 0.2rem;
-    }
-
     &-light{
       position: absolute;  
       top: 75px;
