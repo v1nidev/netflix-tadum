fb79b75cfd2c402ceec7c7468faaea9e61e4f0b5
Matheus Casante <matheus.casante@gmail.com>
2020-10-19 04:58:19 -0300

    Change follow approach

--- frontend/components/emicida/EmicidaFollow/index.vue
+++ frontend/components/emicida/EmicidaFollow/index.vue
@@ -1,14 +1,25 @@
 <template>
   <section class="carousel">
     <h3 class="has-text-centered subtitle" v-html="content.title" />
-    <CardSlider :queries="slider_config" class="card-box m-b-50 columns is-mobile is-flex is-centered">
+    <!-- <CardSlider :queries="slider_config" class="card-box m-b-50 columns is-mobile is-flex is-centered">
       <div v-for="(card, index) in content.cards" :key="index" class="column is-narrow slider-item p-12">
         <div class="generic-card">
           <ResponsiveImage :sources="card.image.src" :lazyload="false" />
           <p class="has-text-centered-mobile m-x-8" v-html="card.description" />
         </div>
       </div>
-    </CardSlider>
+    </CardSlider> -->
+
+    <div class="scroll-x">
+      <div class="follow-holder m-x-a">
+        <div v-for="(card, index) in content.cards" :key="index">
+          <div class="generic-card">
+            <ResponsiveImage :sources="card.image.src" :lazyload="false" />
+            <p class="has-text-centered-mobile m-x-8" v-html="card.description" />
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -49,32 +60,79 @@ export default {
 <style lang="scss" scoped>
 @import "~assets/css/overrides";
 
-@include until($tablet) {
-  .card-slider {
-    display: block !important;
-  }
+// @include until($tablet) {
+//   .card-slider {
+//     display: block !important;
+//   }
 
-  .slider-item {
-    flex-shrink: unset;
-  }
+//   .slider-item {
+//     flex-shrink: unset;
+//   }
 
-  /deep/.flickity-slider {
-    width: 100%;
+//   /deep/.flickity-slider {
+//     width: 100%;
 
-    .slider-item {
-      &:first-child {
-        width: 390px;
-      }
+//     .slider-item {
+//       &:first-child {
+//         width: 390px;
+//       }
 
-      &:last-child {
-        width: 580px;
-      }
-    }
-  }
+//       &:last-child {
+//         width: 580px;
+//       }
+//     }
+//   }
 
-  .slider-item {
-    padding: 0 !important;
-    width: 100%;
+//   .slider-item {
+//     padding: 0 !important;
+//     width: 100%;
+
+//     .generic-card {
+//       width: auto;
+//       background: $gray-light;
+//       border-top: 2px solid #000;
+//       border-bottom: 2px solid #000;
+//       display: flex;
+//       overflow: hidden;
+//       height: 232px;
+//       padding: 20px;
+
+//       /deep/img {
+//         width: 138px;
+//         max-width: unset
+//       }
+
+//       p {
+//         width: 100%;
+//       }
+
+//     }
+    
+//     &:not(:last-child) .generic-card {
+//       border-right: 2px dashed #000;
+//     }
+
+//     &:first-child .generic-card {
+//       border-left: 2px solid #000;
+//       border-radius: 20px 0 0 20px;
+//       margin-left: 30px;
+//     }
+
+//     &:last-child .generic-card {
+//       border-right: 2px solid #000;
+//       border-radius: 0 20px 20px 0;
+//       margin-right: 30px;
+//     }
+//   }
+// }
+
+
+.scroll-x {
+  overflow-x: scroll;
+
+  .follow-holder {
+    display: flex;
+    width: calc(390px + 580px);
 
     .generic-card {
       width: auto;
@@ -97,24 +155,35 @@ export default {
 
     }
     
-    &:not(:last-child) .generic-card {
-      border-right: 2px dashed #000;
+    > div {
+      display: flex;
+
+      &:not(:last-child) .generic-card {
+        border-right: 2px dashed #000;
+      }
+
+      &:first-child .generic-card {
+        border-left: 2px solid #000;
+        border-radius: 20px 0 0 20px;
+        margin-left: 30px;
+      }
+
+      &:last-child .generic-card {
+        border-right: 2px solid #000;
+        border-radius: 0 20px 20px 0;
+        margin-right: 30px;
+      }
     }
 
-    &:first-child .generic-card {
-      border-left: 2px solid #000;
-      border-radius: 20px 0 0 20px;
-      margin-left: 30px;
+    > div:first-child {
+      width: 390px;
     }
 
-    &:last-child .generic-card {
-      border-right: 2px solid #000;
-      border-radius: 0 20px 20px 0;
-      margin-right: 30px;
+    > div:last-child {
+      width: 580px;
     }
   }
 }
-
 
 
 .carousel{
