6242c6ec28d0c03816dc35e11f6d4605138939f8
Olavo Parno <olavoparno@gmail.com>
2020-10-20 10:49:37 -0300

    refactor(me-n-my-loves): adjust desktop layout

--- frontend/components/eu-e-meus-amores/index.vue
+++ frontend/components/eu-e-meus-amores/index.vue
@@ -1,26 +1,26 @@
 <template>
   <main class="eu-e-meus-amores is-clipped p-l-16 p-r-16">
-    <div class="container">
-      <h1 class="title">
-        {{ title }}
-      </h1>
-      <h2 class="description has-text-pink">
-        {{ description }}
-      </h2>
-      
-      <article class="box-container">
-        <section class="section-container">
-          <ResponsiveImage
-            :sources="looseCard.imageSet.src"
-            :lazyload="false"
-            :alt="looseCard.imageSet.alt"
-          />
-          <header class="section-header-text">
-            Quando a gente divide o tempo entre crush e migues, 24horas parece pouco, né? Mas bora lá que esse problema é do tipo “vira essa boca pra cá”. Se organizar direitinho, isso só acontece nas séries e filmes.
-          </header>
-        </section>
-      </article>
+    <h1 class="title p-t-48">
+      {{ title }}
+    </h1>
+    <h2 class="description has-text-pink">
+      {{ description }}
+    </h2>
+    
+    <article class="box-container">
+      <section class="section-container">
+        <ResponsiveImage
+          :sources="looseCard.imageSet.src"
+          :lazyload="false"
+          :alt="looseCard.imageSet.alt"
+        />
+        <header class="section-header-text entry">
+          Quando a gente divide o tempo entre crush e migues, 24horas parece pouco, né? Mas bora lá que esse problema é do tipo “vira essa boca pra cá”. Se organizar direitinho, isso só acontece nas séries e filmes.
+        </header>
+      </section>
+    </article>
 
+    <div class="container">
       <CardsList :cardsList="cardsList" />
 
       <Share text-class="share-text" button-class="share-button" />
@@ -74,14 +74,28 @@ export default {
   color: black;
 
   & .container {
-    margin-top: $size-5 + 4;
+    margin-top: 40px;
+    padding: 0 !important;
+
+    @media (min-width: $desktop) {
+      & > div {
+        margin: auto;
+        max-width: 667px;
+      }
+    }
   }
 
   & /deep/.box-container {
-    border-radius: $size-7;
+    border-radius: 20px;
     & img {
-      border-radius: $size-7;
+      border-radius: 20px;
       border: 1.2px solid black;
+      width: 100%;
+
+      @media (min-width: $desktop) {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+      }
     }
   }
 
@@ -95,18 +109,30 @@ export default {
 
   & .description {
     margin-bottom: 2rem;
-    font-size: $size-3 + 10;
+    font-size: 80px;
     font-family: Agipo;
     font-weight: bold;
     line-height: 70px;
     letter-spacing: -0.01em;
     -webkit-text-stroke-width: 1.5px;
     -webkit-text-stroke-color: black;
+
+    @media (min-width: $desktop) {
+      font-size: 150px;
+      line-height: 83.5%;
+      letter-spacing: -0.03em;
+    }
   }
 
   & .section-container {
-    max-width: 1000px;
     margin: auto;
+
+    @media (min-width: $desktop) {
+      & /deep/ img {
+        display: block;
+      }
+    }
+
     & .section-header-text {
       margin: ($size-7 + 5) ($size-8 - 6) $size-5;
       font-size: 17px;
@@ -121,6 +147,15 @@ export default {
 
       @media (min-width: $desktop) {
         padding: 0  $size-5;
+
+        &.entry {
+          margin: 0 0 100px;
+          padding: 35px 190px;
+          background-color: $pink;
+          border: 1.5px solid black;
+          border-bottom-left-radius: 20px;
+          border-bottom-right-radius: 20px;
+        }
       }
     }
   }
